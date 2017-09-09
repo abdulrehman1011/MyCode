21224cb0cbd9cfda8fe5b6642951c122d76c71ba
Abdul Rehman <abdul.rehman@apollo.com.pk>
2017-09-09 12:52:55 +0500

    s

---
+++ mymediaplayer/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace mymediaplayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.btnPlayPromt = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.Color.Transparent;
+            this.btnBrowse.Location = new System.Drawing.Point(46, 83);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse..";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(31, 32);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(144, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Text to Speech";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNo
+            // 
+            this.txtNo.Location = new System.Drawing.Point(31, 138);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(100, 20);
+            this.txtNo.TabIndex = 4;
+            // 
+            // btnPlayPromt
+            // 
+            this.btnPlayPromt.Location = new System.Drawing.Point(148, 136);
+            this.btnPlayPromt.Name = "btnPlayPromt";
+            this.btnPlayPromt.Size = new System.Drawing.Size(75, 23);
+            this.btnPlayPromt.TabIndex = 5;
+            this.btnPlayPromt.Text = "Play Promt";
+            this.btnPlayPromt.UseVisualStyleBackColor = true;
+            this.btnPlayPromt.Click += new System.EventHandler(this.btnPlayPromt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 204);
+            this.Controls.Add(this.btnPlayPromt);
+            this.Controls.Add(this.txtNo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.btnBrowse);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MyMediaPlayer";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.Button btnPlayPromt;
+    }
+}
+
---
+++ mymediaplayer/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace mymediaplayer
+{
+    public partial class Form1 : Form
+    {
+        WMPLib.WindowsMediaPlayer Player = new WMPLib.WindowsMediaPlayer();
+        WMPLib.WindowsMediaPlayer Player1 = new WMPLib.WindowsMediaPlayer();
+        List<string> strArr;
+        int len = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "mp3,wav,mp4,mov,wmv,mpg|*.mp3;*.wav;*.mp4;*.mov;*.wmv;*.mpg|all files|*.*";
+            openFileDialog1.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory.ToString();
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                //axWindowsMediaPlayer1.URL = openFileDialog1.FileName;
+                Player.URL = openFileDialog1.FileName;
+                Player.settings.volume = 70;
+                Player.controls.play();
+                
+            }
+                
+           
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            Player.settings.volume = trackBar1.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.Show();
+        }
+
+        private void btnPlayPromt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                //C:\Users\Abdul Rehman\Desktop\Apollo\Demo WMV+TTS\mymediaplayer\bin\Debug\WAV files\0.wav
+                string fl = txtNo.Text.Trim();
+                strArr = new List<string>();
+                if(File.Exists(fl))
+                {
+                    string path = AppDomain.CurrentDomain.BaseDirectory + "WAV files\\" + fl + ".wav";
+                    Player.URL = path;
+                    Player.settings.volume = 90;
+                    Player.controls.play();
+                }
+                else
+                {
+                    decimal p = 0;
+                    p = Math.Truncate(Convert.ToDecimal(fl));
+                    decimal d1 = Convert.ToDecimal(fl);
+                    p = d1 - p;
+                    p = Math.Round(p, 2);
+                    long num = Convert.ToInt64(Math.Truncate(Convert.ToDouble(fl)));
+                    long i = 10;
+                    long i2 = 0;
+                    List<string> strArr1 = new List<string>(); ;
+                    
+                        while (num > i / 10)
+                        {
+                            i2 = num % i - num % (i / 10);
+                            if (i2 > 0)
+                            {
+                                strArr.Add(i2.ToString());
+                            }
+
+                            i *= 10;
+                            /*if ((num % i - num % (i / 10)) >= 10 && (num % i - num % (i / 10)) < 20)
+                            {
+                                strArr.RemoveAt(strArr.Count - 1);
+                                if ((num % i - num % (i / 10)) > 0)
+                                {
+                                    strArr.Add((i2 + (num % i - num % (i / 10))).ToString());
+                                }
+                            
+                                i *= 10;
+                            }*/
+
+                        }
+                        for (int k = strArr.Count-1; k >=0 ; k--)
+                    {
+                        if (strArr[k].Length == 6)
+                        {
+                            int m = Convert.ToInt32(strArr[k]);
+                            m = m / 100000;
+                            strArr[k] = m.ToString();
+                            if (strArr[k - 1].Length == 5 || strArr[k - 1].Length == 4)
+                            {
+                                strArr.Insert(k, "hundred");
+                            }
+                            else
+                            {
+                                strArr.Insert(k, "hundred");
+                                strArr.Insert(k, "thousand");
+                            }
+                            
+                            continue;
+
+                        }
+                        if (strArr[k].Length == 5)
+                        {
+                                int m = Convert.ToInt32(strArr[k]);
+                                m = m / 10000;
+                                strArr[k] = m.ToString();
+                                if (k-1 >=0 && strArr[k - 1].Length == 4)
+                                {
+                                    strArr[k] = strArr[k] + "" + strArr[k - 1].Substring(0, 1);
+                                    strArr.RemoveAt(k - 1);
+                                    strArr.Insert(k - 1, "thousand");
+                                }
+                                else
+                                {
+                                    strArr[k] = strArr[k]+"0";
+                                    strArr.Insert(k, "thousand");
+                                }
+                                
+                                continue;
+
+                        }
+                        if (strArr[k].Length == 4)
+                        {
+                            int m = Convert.ToInt32(strArr[k]);
+                            m = m / 1000;
+                            strArr[k] = m.ToString();
+                            strArr.Insert(k, "thousand");
+                            continue;
+                        }
+                        if (strArr[k].Length == 3)
+                        {
+                            int m = Convert.ToInt32(strArr[k]);
+                            m = m / 100;
+                            strArr[k] = m.ToString();
+                            strArr.Insert(k, "hundred");
+                        
+                            continue;
+                        }
+                        if (strArr[k].Length == 2)
+                        {
+                            if((k-1) >=0 &&strArr[k-1].Length == 1 )
+                            {
+                                int d = Convert.ToInt32(strArr[k]);
+                                d = d / 10;
+                                strArr[k] = d.ToString();
+                                strArr[k] = strArr[k] + "" + strArr[k - 1];
+                                strArr.RemoveAt(k - 1);
+                            }
+                            
+                            continue;
+                        }
+                    }
+                    if(p>0)
+                    {
+                        string tr = p.ToString("0.00");
+                        strArr.Insert(0, "--dollars");
+                        strArr.Insert(0, "and");
+                        strArr.Insert(0,p.ToString("0.00").Substring(2, 2));
+                        strArr.Insert(0,"--cent");
+                       
+                    }
+                    else
+                    {
+                        Regex regex = new Regex(@"^\d$");
+                        try
+                        {
+
+                            double f = Convert.ToDouble(strArr[0]);
+                            if (strArr.Count > 2)
+                            {
+                                strArr.Insert(1, "and");
+                            }
+                            strArr.Insert(0, "--dollars");
+                           
+                            
+                        }
+                        catch(Exception ex)
+                        {
+                            if (strArr.Count > 2)
+                            {
+                                strArr.Insert(2, "and");
+                            }
+                            strArr.Insert(0, "--dollars");
+                            //strArr.Insert(2, "and");
+                        }
+                        
+                       
+                       
+                    }
+                    
+                    var plist = Player1.playlistCollection.newPlaylist("myPL");
+                   
+
+                    string pt = AppDomain.CurrentDomain.BaseDirectory + "eng_sayitsmart\\" + strArr[strArr.Count - 1] + ".wav";
+                    Player1.URL = pt;
+                    strArr.RemoveAt(strArr.Count - 1);
+                    Player1.PlayStateChange+= new WMPLib._WMPOCXEvents_PlayStateChangeEventHandler(str33);
+                    
+                    //var mediaItem1 = Player1.newMedia(AppDomain.CurrentDomain.BaseDirectory + "WAV files\\" + strArr[strArr.Count-1] + ".wav");
+                    //plist.removeItem(mediaItem1);
+
+
+                   //not necessary
+                    
+                    //Player1.currentPlaylist = plist;
+                    
+                }
+                
+                
+               
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Text formate= is wrong");
+            }
+            
+        }
+
+        private void str33(int NewState)
+        {
+            if (Player1.playState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                if (strArr.Count > 0)
+                {
+                    string pt = AppDomain.CurrentDomain.BaseDirectory + "eng_sayitsmart\\" + strArr[strArr.Count - 1] + ".wav";
+                    Player1 = new WMPLib.WindowsMediaPlayer();
+                    Player1.URL = pt;
+                    
+                    strArr.RemoveAt(strArr.Count - 1);
+                    Player1.PlayStateChange += new WMPLib._WMPOCXEvents_PlayStateChangeEventHandler(str33);
+                }
+
+            }
+           
+        }
+
+        
+
+        
+
+
+
+        
+    }
+}
